02af0dfd0eed87e8427c87e1c3080773d1b62fad
CuperL <1309184729@qq.com>
2017-07-24 18:52:08 +0800

    非递归的ClimbingStairs

---
+++ ClimbingStairs.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+	int climbStairs(int n) {
+		if (n == 1) return 1;
+		else if (n == 2) return 2;
+		else
+		{
+			int *a;
+			a = new int[n];
+			a[0] = 1;
+			a[1] = 2;
+			for (int i = 2; i<n; i++)
+				a[i] = a[i - 1] + a[i - 2];
+			return a[n - 1];
+			delete[]a;
+		}
+
+	}
+};
